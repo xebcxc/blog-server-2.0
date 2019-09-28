3dccbbfd5be317fc387521f64ddf726bff6da758
xebcxc <ms915818993@163.com>
2019-09-28 12:40:32 +0800

    修复上传文件的问题

--- src/main/java/pro/meisen/boot/web/controller/StaticController.java
+++ src/main/java/pro/meisen/boot/web/controller/StaticController.java
@@ -1,18 +1,18 @@
 package pro.meisen.boot.web.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.logging.log4j.util.Strings;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pro.meisen.boot.core.exception.AppException;
 import pro.meisen.boot.domain.common.ErrorCode;
-import pro.meisen.boot.helper.DateHelper;
+import pro.meisen.boot.ext.shiro.encrypt.MD5HashEncryptor;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.UUID;
 
 /**
  * 静态处理器
@@ -21,15 +21,14 @@ import java.util.UUID;
  */
 @RestController
 @RequestMapping("/api/static")
+@Api(description = "上传文件", tags = "上传文件Tag")
 public class StaticController {
 
     @Value("${web.upload-path}")
     private String path;
 
-    @Autowired
-    private DateHelper dateHelper;
-
     @PostMapping("/file/upload")
+    @ApiOperation(notes = "上传文件", value = "上传文件")
     public @ResponseBody
     String fileUpload(@RequestParam("file") MultipartFile file) {
         String fileName = file.getOriginalFilename();
@@ -37,7 +36,7 @@ public class StaticController {
             throw new AppException(ErrorCode.APP_FILE_UPLOAD_FAIL, "文件名错误, 请重新上传.");
         }
         String fileExt = fileName.substring(fileName.lastIndexOf('.'));
-        String encryptName = UUID.fromString(fileName) + fileExt;
+        String encryptName = new MD5HashEncryptor.Builder(fileName).build().toString() + fileExt;
         // 文件路径需要根据上线之后的路径确定
         String filePath = path + encryptName;
         try {
